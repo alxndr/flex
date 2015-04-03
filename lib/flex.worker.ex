ae4ae8f9133130b56645436f510d0ad30d9f5351
Alexander <alxndr+github@gmail.com>
2015-04-02 19:07:19 -0700

    extract conversion options

--- lib/flex.worker.ex
+++ lib/flex.worker.ex
@@ -15,22 +15,9 @@ defmodule Flex.Worker do
     mp3file = Path.join(dirname, "#{basename}.mp3")
 
     # synchronous
-    System.cmd "flac", [
-      "--silent",
-      "--force",
-      "--decode",
-      "--output-name",
-      wavfile,
-      flacfile
-    ], stderr_to_stdout: false
+    System.cmd "flac", conversion_options(:flac, wavfile, flacfile), stderr_to_stdout: false
     IO.write "."
-    System.cmd "lame", [
-      "--silent",
-      "--#{@lame_options[:type]}",
-      "#{@lame_options[:bitrate]}",
-      wavfile,
-      mp3file
-    ], stderr_to_stdout: false
+    System.cmd "lame", conversion_options(:lame, wavfile, mp3file), stderr_to_stdout: false
     System.cmd "rm", [wavfile]
 
     IO.puts "finished #{basename}"
@@ -47,6 +34,27 @@ defmodule Flex.Worker do
     basename = Path.basename(filename, ".flac")
     dirname = Path.dirname(filename)
     {basename, dirname}
+  end
+
+  @spec conversion_options(atom, String.t, String.t) :: [String.t]
+  def conversion_options(:flac, wavfile, flacfile) do
+    [
+      "--silent",
+      "--force",
+      "--decode",
+      "--output-name",
+      wavfile,
+      flacfile
+    ]
+  end
+  def conversion_options(:lame, wavfile, mp3file) do
+    [
+      "--silent",
+      "--#{@lame_options[:type]}",
+      "#{@lame_options[:bitrate]}",
+      wavfile,
+      mp3file
+    ]
   end
 
 end
