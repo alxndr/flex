027a398ee4397ff430cbc56dd510c157a78aabb3
Alexander <alxndr+github@gmail.com>
2015-04-06 22:23:28 -0700

    fix how dependency check uses Porcelain

--- lib/flex.ex
+++ lib/flex.ex
@@ -5,6 +5,8 @@ defmodule Flex do
   Finds .flac files and creates corresponding .wav and then .mp3 files from them.
   """
 
+  alias Porcelain.Result
+
   @sec 1_000
   @file_conversion_timeout 30 * @sec
 
@@ -22,20 +24,20 @@ defmodule Flex do
       |> Enum.map(&Task.async(Worker, :convert_flac, [&1]))
       |> Enum.map(&Task.await(&1, @file_conversion_timeout))
       |> Enum.map(&IO.puts "\nConverted: #{&1}")
-      |> Enum.count
+      |> Enum.count()
 
     IO.puts "\nConverted #{count} files"
   end
 
   defp check_dependencies do
     case Porcelain.exec("which", ["flac"]) do
-      {_, 0} -> true
+      %Result{status: 0} -> true
       _ ->
         IO.puts "need flac in $PATH"
         System.halt(1)
     end
     case Porcelain.exec("which", ["lame"]) do
-      {_, 0} -> true
+      %Result{status: 0} -> true
       _ ->
         IO.puts "need lame in $PATH"
         System.halt(1)
