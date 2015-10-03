1ae905fefe792eb62ce64c79566fee700c4ebb2f
Alexander <alxndr+github@gmail.com>
2015-10-03 14:03:43 -0500

    custom ruleset tooling

--- lib/dogma_rule_set.ex
+++ lib/dogma_rule_set.ex
@@ -1,19 +1,31 @@
 defmodule Flex.DogmaRuleSet do
+
   @behaviour Dogma.RuleSet
+  @custom_rules [
+    # to disable a rule, put `false` as the 2nd element in the tuple
+    {LineLength, max_length: 100},
+    # {ModuleDoc, false},
+  ]
 
   def rules do
     Dogma.RuleSet.All.rules
-    |> IO.inspect
-    |> Enum.filter(&rule_filter/1)
-    |> Enum.concat([{LineLength, max_length: 100}]) # could that be a keyword list?
+    |> Enum.filter(&(!overridden_rule?(&1)))
+    |> Enum.concat(remove_disabled_rules(@custom_rules))
   end
 
-  defp rule_filter(rule) do
-    IO.puts "rule_filter called..."
-    case rule do # TODO multiple fn defs
-      {ModuleDoc}     -> false
-      {LineLength, _} -> false
-      _               -> true
-    end
+  defp overridden_rule?(rule) do
+    rule_name = get_rule_name(rule)
+    Enum.any? @custom_rules, &(get_rule_name(&1) == rule_name)
   end
+
+  defp remove_disabled_rules(custom_rules) do
+    custom_rules
+    |> Enum.filter(fn
+      {_, false} -> false
+      _          -> true
+    end)
+  end
+
+  defp get_rule_name({name}), do: name
+  defp get_rule_name({name, _}), do: name
 end
