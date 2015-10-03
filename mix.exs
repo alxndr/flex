f56918639404c33252e41dc37a5e34e00bc2e555
Alexander <alxndr+github@gmail.com>
2015-10-03 14:55:07 -0500

    expose dogma in test env

--- mix.exs
+++ mix.exs
@@ -21,9 +21,9 @@ defmodule Flex.Mixfile do
 
   defp deps do
     [
-      {:dogma, "~> 0.0.8", only: :dev},
+      {:dogma, "~> 0.0.8", only: [:dev, :test]},
       {:inch_ex, only: :docs},
-      {:porcelain, "~> 2.0"}, # install goon too
+      {:porcelain, "~> 2.0"},
     ]
   end
 
